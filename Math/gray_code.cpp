ac290740eba75faaff185b1cdc4f0fd13d1d72a4
Aryan <aryanshroff21@gmail.com>
2023-06-09 15:02:32 +0530

    gray_code

---
+++ Math/gray_code.cpp
@@ -0,0 +1,155 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+
+template <typename T>
+void print1d(vector<T> v)
+{
+    cout << " 1D VECTOR " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void print2d(vector<vector<T>> v)
+{
+    cout << " 2D VECTOR " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void printstack(stack<T> s)
+{
+    cout << " STACK " << endl;
+    while (s.empty() == false)
+    {
+        cout << s.top() << endl;
+        s.pop();
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void printq(queue<T> q)
+{
+    cout << " QUEUE " << endl;
+    while (q.empty() == false)
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <class K, class V>
+void printhash(unordered_map<K, V> h)
+{
+    cout << " HASHMAP " << endl;
+    for (auto it = h.begin(); it != h.end(); it++)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+    cout << endl;
+    cout << endl;
+}
+
+// 2d vec of charecter to 2d vec of int
+void generate(string s)
+{
+    string res;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '[')
+        {
+            res.push_back('{');
+        }
+        else if (s[i] == ']')
+        {
+            res.push_back('}');
+        }
+        else
+        {
+            res.push_back(s[i]);
+        }
+    }
+    cout << res << endl;
+    cout << endl;
+}
+
+//************************************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+vector<int> grayCode(int n)
+{
+    if(n==1){
+        vector<int> res={0,1};
+        return res;
+    }
+    else if(n==0){
+        vector<int> res={0};
+        return res;
+    }
+    else{
+        //reached here means n is greater than 1
+        vector<int> res=grayCode(n-1);
+        int power=n-1;
+        int base=2;
+        int ans=pow(base, power);
+        int len=res.size();
+        for(int i=len-1; i>=0; i--){
+            int val=res[i];
+            val+=ans;
+            res.push_back(val);
+        }
+        return res;
+
+    }
+
+}
+
+void mymain()
+{
+    int n=4;
+    vector<int> res=grayCode(n);
+    print1d(res);
+}
+
+//************************************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+
+int main()
+{
+    // take all input here
+
+    // input end
+    chrono::system_clock::time_point start = high_resolution_clock::now();
+    mymain();
+    chrono::system_clock::time_point stop = high_resolution_clock::now();
+    chrono::milliseconds duration = duration_cast<milliseconds>(stop - start);
+
+    double time = duration.count();
+    string unit = "ms";
+    if (time >= 1000 and time < 60000)
+    {
+        time = time / 1000;
+        unit = "sec";
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Code Execution Time: " << time << " " << unit << endl;
+    return 0;
+}
