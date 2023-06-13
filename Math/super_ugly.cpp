9527b44a6a197d25662adbcbca80146e60414893
Aryan <aryanshroff21@gmail.com>
2023-06-13 22:01:28 +0530

    math

---
+++ Math/super_ugly.cpp
@@ -0,0 +1,154 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+
+template <typename T>
+void print1d(vector<T> v)
+{
+    cout << " 1D VECTOR " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void print2d(vector<vector<T>> v)
+{
+    cout << " 2D VECTOR " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void printstack(stack<T> s)
+{
+    cout << " STACK " << endl;
+    while (s.empty() == false)
+    {
+        cout << s.top() << endl;
+        s.pop();
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void printq(queue<T> q)
+{
+    cout << " QUEUE " << endl;
+    while (q.empty() == false)
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <class K, class V>
+void printhash(unordered_map<K, V> h)
+{
+    cout << " HASHMAP " << endl;
+    for (auto it = h.begin(); it != h.end(); it++)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+    cout << endl;
+    cout << endl;
+}
+
+// 2d vec of charecter to 2d vec of int
+void generate(string s)
+{
+    string res;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '[')
+        {
+            res.push_back('{');
+        }
+        else if (s[i] == ']')
+        {
+            res.push_back('}');
+        }
+        else
+        {
+            res.push_back(s[i]);
+        }
+    }
+    cout << res << endl;
+    cout << endl;
+}
+
+//************************************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+int nthSuperUglyNumber(int n, vector<int>& primes)
+{
+    priority_queue<long long,vector<long long>,greater<long long>> pq;
+    int count=0;
+    long long curr;
+    long long top=0;
+    pq.push(1);
+    unordered_set<int> h;
+    while(count!=n){
+        top=pq.top();
+        cout<<top<<endl;
+        pq.pop();
+        count++;
+        for(int i=0;i<primes.size();i++){
+            curr=primes[i]*top;
+            if(curr>INT_MAX) continue;
+            if(h.find(curr)==h.end()){
+                h.insert(curr);
+                pq.push(curr);
+            }
+        }
+        
+
+    }
+    return top;
+}
+
+void mymain()
+{
+    vector<int> v={2,7,13,19};
+    int result = nthSuperUglyNumber(100000,v);
+    cout << "result: " << result << endl;
+}
+
+//************************************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+
+int main()
+{
+    // take all input here
+
+    // input end
+    chrono::system_clock::time_point start = high_resolution_clock::now();
+    mymain();
+    chrono::system_clock::time_point stop = high_resolution_clock::now();
+    chrono::milliseconds duration = duration_cast<milliseconds>(stop - start);
+
+    double time = duration.count();
+    string unit = "ms";
+    if (time >= 1000 and time < 60000)
+    {
+        time = time / 1000;
+        unit = "sec";
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Code Execution Time: " << time << " " << unit << endl;
+    return 0;
+}
---
+++ Math/trailing_zero.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+
+template <typename T>
+void print1d(vector<T> v)
+{
+    cout << " 1D VECTOR " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void print2d(vector<vector<T>> v)
+{
+    cout << " 2D VECTOR " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void printstack(stack<T> s)
+{
+    cout << " STACK " << endl;
+    while (s.empty() == false)
+    {
+        cout << s.top() << endl;
+        s.pop();
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void printq(queue<T> q)
+{
+    cout << " QUEUE " << endl;
+    while (q.empty() == false)
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <class K, class V>
+void printhash(unordered_map<K, V> h)
+{
+    cout << " HASHMAP " << endl;
+    for (auto it = h.begin(); it != h.end(); it++)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+    cout << endl;
+    cout << endl;
+}
+
+// 2d vec of charecter to 2d vec of int
+void generate(string s)
+{
+    string res;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '[')
+        {
+            res.push_back('{');
+        }
+        else if (s[i] == ']')
+        {
+            res.push_back('}');
+        }
+        else
+        {
+            res.push_back(s[i]);
+        }
+    }
+    cout << res << endl;
+    cout << endl;
+}
+
+//************************************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+int trailingZeroes(int n)
+{
+    int count=0;
+    int base=5;
+    while(base<=n){
+        count+=n/base;
+        //cout<<count<<endl;
+        base*=5;
+    }
+    return count;
+}
+
+void mymain()
+{
+    
+    int result = trailingZeroes(300);
+    cout << "result: " << result << endl;
+}
+
+//************************************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+
+int main()
+{
+    // take all input here
+
+    // input end
+    chrono::system_clock::time_point start = high_resolution_clock::now();
+    mymain();
+    chrono::system_clock::time_point stop = high_resolution_clock::now();
+    chrono::milliseconds duration = duration_cast<milliseconds>(stop - start);
+
+    double time = duration.count();
+    string unit = "ms";
+    if (time >= 1000 and time < 60000)
+    {
+        time = time / 1000;
+        unit = "sec";
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Code Execution Time: " << time << " " << unit << endl;
+    return 0;
+}
