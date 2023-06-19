1a9f65113980b5568d7d85ae03caacc59dc1130b
Aryan <aryanshroff21@gmail.com>
2023-06-19 10:25:37 +0530

    basic categoray of leetcode

---
+++ Basic/highestAltitude.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+
+template <typename T>
+void print1d(vector<T> v)
+{
+    cout << " 1D VECTOR " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void print2d(vector<vector<T>> v)
+{
+    cout << " 2D VECTOR " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void printstack(stack<T> s)
+{
+    cout << " STACK " << endl;
+    while (s.empty() == false)
+    {
+        cout << s.top() << endl;
+        s.pop();
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void printq(queue<T> q)
+{
+    cout << " QUEUE " << endl;
+    while (q.empty() == false)
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <class K, class V>
+void printhash(unordered_map<K, V> h)
+{
+    cout << " HASHMAP " << endl;
+    for (auto it = h.begin(); it != h.end(); it++)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+    cout << endl;
+    cout << endl;
+}
+
+// 2d vec of charecter to 2d vec of int
+void generate(string s)
+{
+    string res;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '[')
+        {
+            res.push_back('{');
+        }
+        else if (s[i] == ']')
+        {
+            res.push_back('}');
+        }
+        else
+        {
+            res.push_back(s[i]);
+        }
+    }
+    cout << res << endl;
+    cout << endl;
+}
+
+//************************************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+int largestAltitude(vector<int> &gain)
+{
+    int maxValue = 0, curr = 0;
+    for (int i = 0; i < gain.size(); i++)
+    {
+        curr += gain[i];
+        if (curr > maxValue)
+        {
+            maxValue = curr;
+        }
+    }
+    return maxValue;
+}
+
+void mymain()
+{
+    vector<int> gain = {-4, -3, -2, -1, 4, 3, 2};
+    int result = largestAltitude(gain);
+    cout << "result: " << result << endl;
+}
+
+//************************************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+
+int main()
+{
+    // take all input here
+
+    // input end
+    chrono::system_clock::time_point start = high_resolution_clock::now();
+    mymain();
+    chrono::system_clock::time_point stop = high_resolution_clock::now();
+    chrono::milliseconds duration = duration_cast<milliseconds>(stop - start);
+
+    double time = duration.count();
+    string unit = "ms";
+    if (time >= 1000 and time < 60000)
+    {
+        time = time / 1000;
+        unit = "sec";
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Code Execution Time: " << time << " " << unit << endl;
+    return 0;
+}
