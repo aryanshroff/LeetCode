6d684a478d28f198e589f92ea9d7c1f7e38efba6
Aryan <aryanshroff21@gmail.com>
2023-06-22 19:11:16 +0530

    added dp problems

---
+++ Math/makeNumberBeautiful.cpp
@@ -0,0 +1,163 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+
+template <typename T>
+void print1d(vector<T> v)
+{
+    cout << " 1D VECTOR " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void print2d(vector<vector<T>> v)
+{
+    cout << " 2D VECTOR " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void printstack(stack<T> s)
+{
+    cout << " STACK " << endl;
+    while (s.empty() == false)
+    {
+        cout << s.top() << endl;
+        s.pop();
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void printq(queue<T> q)
+{
+    cout << " QUEUE " << endl;
+    while (q.empty() == false)
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <class K, class V>
+void printhash(unordered_map<K, V> h)
+{
+    cout << " HASHMAP " << endl;
+    for (auto it = h.begin(); it != h.end(); it++)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+    cout << endl;
+    cout << endl;
+}
+
+// 2d vec of charecter to 2d vec of int
+void generate(string s)
+{
+    string res;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '[')
+        {
+            res.push_back('{');
+        }
+        else if (s[i] == ']')
+        {
+            res.push_back('}');
+        }
+        else
+        {
+            res.push_back(s[i]);
+        }
+    }
+    cout << res << endl;
+    cout << endl;
+}
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// call generate function to generate 2d vector from string s
+
+//************************************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+long long makeIntegerBeautiful(long long n, int target)
+{
+    long long cost = 0;
+    //more the zeros better its for us
+    long long n2=n;
+    int sum=0;
+    while(n>0){
+        sum+=n%10;
+        n=n/10;
+    }
+    n=n2;
+    int i=0,last=0;
+    long long diff=0;
+    while(sum>target){
+        last=n%10;
+        n=n/10;
+        n++;
+        diff=10-last;
+        diff=diff*pow(10,i);
+        cost+=diff;
+        sum-=last;
+        sum++;
+        i++;
+    }
+    return cost;
+}
+
+void mymain()
+{
+    string s = "";
+    long long result=makeIntegerBeautiful(476,6);
+    cout << "result: " << result << endl;
+}
+
+//************************************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+
+int main()
+{
+    chrono::system_clock::time_point start = high_resolution_clock::now();
+    mymain();
+    chrono::system_clock::time_point stop = high_resolution_clock::now();
+    chrono::milliseconds duration = duration_cast<milliseconds>(stop - start);
+
+    double time = duration.count();
+    string unit = "ms";
+    if (time >= 1000 and time < 60000)
+    {
+        time = time / 1000;
+        unit = "sec";
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Code Execution Time: " << time << " " << unit << endl;
+    return 0;
+}
