1b742b8e4417d461fbc957bd568f23b2bccbc929
Aryan <aryanshroff21@gmail.com>
2023-06-23 16:55:12 +0530

    binary tree

---
+++ Binary Tree/pathPrint.cpp
@@ -0,0 +1,190 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+
+template <typename T>
+void print1d(vector<T> v)
+{
+    cout << " 1D VECTOR " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void print2d(vector<vector<T>> v)
+{
+    cout << " 2D VECTOR " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void printstack(stack<T> s)
+{
+    cout << " STACK " << endl;
+    while (s.empty() == false)
+    {
+        cout << s.top() << endl;
+        s.pop();
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <typename T>
+void printq(queue<T> q)
+{
+    cout << " QUEUE " << endl;
+    while (q.empty() == false)
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template <class K, class V>
+void printhash(unordered_map<K, V> h)
+{
+    cout << " HASHMAP " << endl;
+    for (auto it = h.begin(); it != h.end(); it++)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+    cout << endl;
+    cout << endl;
+}
+
+// 2d vec of charecter to 2d vec of int
+void generate(string s)
+{
+    string res;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '[')
+        {
+            res.push_back('{');
+        }
+        else if (s[i] == ']')
+        {
+            res.push_back('}');
+        }
+        else
+        {
+            res.push_back(s[i]);
+        }
+    }
+    cout << res << endl;
+    cout << endl;
+}
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// call generate function to generate 2d vector from string s
+
+//************************************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+class Solution
+{
+public:
+    void solve(TreeNode *root, string path, vector<string> &res)
+    {
+        if (root == NULL)
+        {
+            // path is complete
+            res.push_back(path);
+            return;
+        }
+        // recahed here root is not null
+        int val = root->val;
+        string s = "";
+        if (path != "")
+        {
+            s += "->";
+        }
+        s += to_string(val);
+        path += s;
+        // dont make recursive calls on null node untill its a leaf node
+        if (root->left == NULL and root->right == NULL)
+        {
+            // since both children are null we are gonna make a call on any one of them
+
+            solve(root->right, path, res);
+        }
+        else if (root->left == NULL)
+        {
+            // right is not null
+            solve(root->right, path, res);
+        }
+        else if (root->right == NULL)
+        {
+            solve(root->left, path, res);
+        }
+        // reached here means both non null
+        else
+        {
+            solve(root->left, path, res);
+            solve(root->right, path, res);
+        }
+    }
+
+    vector<string> binaryTreePaths(TreeNode *root)
+    {
+        vector<string> res;
+        unordered_set<string> h;
+        string path = "";
+        solve(root, path, res);
+        return res;
+    }
+};
+
+void mymain()
+{
+    string s = "";
+    int result = 0;
+    cout << "result: " << result << endl;
+}
+
+//************************************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+
+int main()
+{
+    chrono::system_clock::time_point start = high_resolution_clock::now();
+    mymain();
+    chrono::system_clock::time_point stop = high_resolution_clock::now();
+    chrono::milliseconds duration = duration_cast<milliseconds>(stop - start);
+
+    double time = duration.count();
+    string unit = "ms";
+    if (time >= 1000 and time < 60000)
+    {
+        time = time / 1000;
+        unit = "sec";
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Code Execution Time: " << time << " " << unit << endl;
+    return 0;
+}
